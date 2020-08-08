08bc98e4f76bbfb7b126011493a2325019951d58
Gustavo Almeida <gustavo_74391@trt5.jus.br>
2020-08-08 15:36:16 -0300

    Corrige erro encoding. Remove analise COVID

--- analise.R
+++ analise.R
@@ -52,74 +52,36 @@ processos <- cnc[,
                  by = processo]
 
 freq = table(cnc$assunto)
-prop = round(prop.table(freq)*100,2) # Construindo as propor??es
-kable(cbind(freq,prop),col.names = c("Freq","Prop")) #Criando tabela de frequ?ncias e propor??es
+prop = round(prop.table(freq)*100,2) # Construindo as proporções
+kable(cbind(freq,prop),col.names = c("Freq","Prop")) #Criando tabela de frequências e proporções
 
 
-# An?lise Vara
+# Análise Vara
 sum(is.na(cnc$vara))
 
 freq = table(cnc$vara) # Construindo a tabela de frequencias 
-prop = round(prop.table(freq)*100,2) # Construindo as propor??es
-kable(cbind(freq,prop),col.names = c("Freq","Prop")) #Criando tabela de frequ?ncias e propor??es
+prop = round(prop.table(freq)*100,2) # Construindo as proporções
+kable(cbind(freq,prop),col.names = c("Freq","Prop")) #Criando tabela de frequências e proporções
 
 xtab <- xtabs(~cnc$vara)
-varas <- names(head(xtab[order(xtab, decreasing = TRUE)], 10)) # Selecionando as varas com maior n?mero
+varas <- names(head(xtab[order(xtab, decreasing = TRUE)], 10)) # Selecionando as varas com maior número
 cnc[, vara_analise := vara] # Crianda outro campo
 cnc[!vara_analise %in% varas, vara_analise := "Outros"] # Renomeando para "Outros" 
 cnc$vara_analise <- factor(cnc$vara_analise, levels = c(varas, "Outros"))  # Definindo a ordem dos fatores do mais frequente para o menos frequente
 
-#rm(xtab, municipios) # Removendo vari?veis que n?o ser?o mais utilizadas
+#rm(xtab, municipios) # Removendo variáveis que não serão mais utilizadas
 
 ggplot(cnc, aes(x=vara_analise)) + 
   geom_bar() + 
-  labs(x="Vara", y="Frequ?ncia", title="CNC") +
+  labs(x="Vara", y="Frequência", title="CNC") +
   theme(axis.text.x = element_text(angle = 45, vjust = 1.0, hjust=1.0))
 
 
-# An?lise Data Autua??o
+# Análise Data Autuação
 sum(is.na(cnc$data_autuacao))
 summary(cnc$data_autuacao)
 
 sum(is.na(cnc$data_concilacao))
 sum(!is.na(cnc$data_concilacao))
 
-cnc[!is.na(data_concilacao)]
-
-# Leitura COVID
-obitos <- read_excel('COVID-19_03-08-2020.xlsx', sheet =  '?bitos')
-obitos <- as.data.table(obitos)
-str(obitos)
-names(obitos)
-
-#Agrupando
-df[, .N, by = "ds_assunto"]
-
-head(df)
-sum(!is.na(df$cep))
-
-#Construindo tabela de frequ?ncia
-freq = table(df$ds_assunto)
-
-prop = round(prop.table(table(df$ds_assunto))*100,2)
-kable(cbind(freq,prop),col.names = c("Freq","Prop"))
-
-ggplot(df[!is.na(data_autuacao)], aes(x=data_autuacao)) + 
-  geom_histogram(bins=40) + 
-  labs(x="Data", y="Frequ?ncia", title="COVID-19")
-
-# Diagrama de dispers?o
-plot(df$data_concilacao)
-
-freq_idade <- table(obitos$idade) 
-moda = names(freq_idade)[freq_idade == max(freq_idade)]
-moda
-
-median(obitos$idade)
-mean(obitos$idade)
-
-# Boxplot
-ggplot(df[!is.na(data_autuacao)], aes(y=data_autuacao)) + 
-  geom_boxplot() + 
-  labs(x="", y="Idade", title="COVID-19")
 
